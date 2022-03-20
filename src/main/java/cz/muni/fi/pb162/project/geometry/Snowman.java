f07233b4a02c038790b576d48d28e268abb473d9
Michaela Lodnanova <xlodnan@fi.muni.cz>
2022-03-20 18:07:41 +0100

    Iteration number four - teraz uz ozaj fixed :-)

--- src/main/java/cz/muni/fi/pb162/project/geometry/Snowman.java
+++ src/main/java/cz/muni/fi/pb162/project/geometry/Snowman.java
@@ -7,8 +7,8 @@ package cz.muni.fi.pb162.project.geometry;
  * @author Michaela Lodnanova
  */
 public class Snowman implements Circular{
-    private static final int CONSTANT_LENGTH = 4;
-    public Circular[] circulars = new Circular[CONSTANT_LENGTH];
+    public static final int CONSTANT_LENGTH = 4;
+    private Circular[] circulars = new Circular[CONSTANT_LENGTH];
     private double factor;
     private static final double CONSTANT_FACTOR = 0.8;
 
